b8b7c498fd5a5d2ebe9c951ef6287e9d0244648a
alzheimeer <fogniebla@hotmail.com>
2020-05-11 23:26:30 -0500

    feature task 4

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody rb;
     public float speed = 2000f;
+    private int score = 0;
     // Update is called once per frame
     void FixedUpdate ()
     {
+        Debug.Log(score);
         if (Input.GetKey("d"))
         {
             rb.AddForce(speed * Time.deltaTime, 0, 0);
@@ -26,4 +28,13 @@ public class PlayerController : MonoBehaviour
             rb.AddForce(0, 0, -speed * Time.deltaTime);
         }
     }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            score += 1;
+        }
+    }
+
 }
--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,16 +1,12 @@
 ﻿using UnityEngine;
 
+
 public class Rotator : MonoBehaviour
 {
-    private int score = 0;
-    void OnTriggerEnter(Collider other)
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-     transform.RotateAround(transform.position, Vector3.up, score + 45 * Time.deltaTime);
+        transform.RotateAround(transform.position, Vector3.up, 45 * Time.deltaTime);
+
     }
+
 }
