c053dac575a5835683f2f2cbc6e62478a80f1620
alzheimeer <fogniebla@hotmail.com>
2020-05-11 20:29:42 -0500

    rotator

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    private int score = 0;
+    void OnTriggerEnter(Collider other)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     transform.RotateAround(transform.position, Vector3.up, score + 45 * Time.deltaTime);
+    }
+}
